3d09ca0716ac6afa024e2d211beb026ef6e1b98c
Tanyababinets <tanya060616@gmail.com>
2024-01-05 08:10:58 +0200

    Add project files.

---
+++ movie10/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace movie10.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } 
+        public string? Title { get; set; }
+        public string? TitleEng { get; set; }
+        public string? Genre { get; set; }
+        public string? Director { get; set; }
+        public int Year { get; set; }
+        public string? Description { get; set; }       
+        public string? Poster { get; set; }
+
+
+
+    }
+}
